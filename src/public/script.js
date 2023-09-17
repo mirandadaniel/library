55c279aad287e6d7273a1f5b0fcd8d7cf76ba26c
mirandadaniel <78826776+mirandadaniel@users.noreply.github.com>
2023-09-17 16:08:07 +0900

    can sort books by author a-z

--- src/public/script.js
+++ src/public/script.js
@@ -1,38 +1,5 @@
-function sortTableAuthor() {
-    console.log('sortTable function called');
+function sortTable() {
     const table = document.querySelector('.table');
-    const sortSelect = document.getElementById('sort');
-
-    const sortByAuthor = (a, b) => {
-        const authorA = a.cells[1].textContent.toLowerCase();
-        const authorB = b.cells[1].textContent.toLowerCase();
-
-        if (authorA < authorB) {
-            return -1;
-        }
-        if (authorA > authorB) {
-            return 1;
-        }
-        return 0;
-    };
-
-    const sortDirection = sortSelect.value;
-    const rows = Array.from(table.tBodies[0].rows); 
-
-    rows.sort(sortByAuthor);
-
-    if (sortDirection === 'desc') {
-        rows.reverse();
-    }
-
-    for (const row of rows) {
-        table.tBodies[0].appendChild(row); 
-    }
-}
-
-function sortTableTitle() {
-    console.log('sortTable function called');
-    const table = document.querySelector('.table'); 
     const sortSelect = document.getElementById('sort');
 
     const sortByAuthor = (a, b) => {
@@ -58,8 +25,184 @@ function sortTableTitle() {
     }
 
     for (const row of rows) {
-        table.tBodies[0].appendChild(row); 
+        table.tBodies[0].appendChild(row);
     }
 }
+
+
+
+// let currentSortDirectionTitle = 'asc'; // Initialize with 'asc' for title sorting
+// let currentSortDirectionAuthor = 'asc'; // Initialize with 'asc' for author sorting
+
+// function sortTableAuthor() {
+//     console.log('sortTableAuthor function called');
+//     const table = document.querySelector('.table');
+//     const sortSelect = document.getElementById('sort');
+
+//     const sortByAuthor = (a, b) => {
+//         const authorA = a.cells[1].textContent.toLowerCase();
+//         const authorB = b.cells[1].textContent.toLowerCase();
+//         if (currentSortDirectionAuthor === 'asc') {
+//             if (authorA < authorB) {
+//                 return -1;
+//             }
+//             if (authorA > authorB) {
+//                 return 1;
+//             }
+//         } else {
+//             if (authorA < authorB) {
+//                 return 1;
+//             }
+//             if (authorA > authorB) {
+//                 return -1;
+//             }
+//         }
+
+//         return 0;
+//     };
+
+//     const sortDirection = sortSelect.value;
+//     const rows = Array.from(table.tBodies[0].rows);
+
+//     rows.sort(sortByAuthor);
+
+//     if (sortDirection === 'desc') {
+//         rows.reverse();
+//         currentSortDirectionAuthor = 'desc'; // Update the sorting direction for author column
+//     } else {
+//         currentSortDirectionAuthor = 'asc'; // Update the sorting direction for author column
+//     }
+
+//     for (const row of rows) {
+//         table.tBodies[0].appendChild(row);
+//     }
+// }
+
+// function sortTableTitle() {
+//     console.log('sortTableTitle function called');
+//     const table = document.querySelector('.table');
+//     const sortSelect = document.getElementById('sort');
+
+//     const sortByTitle = (a, b) => {
+//         const titleA = a.cells[0].textContent.toLowerCase();
+//         const titleB = b.cells[0].textContent.toLowerCase();
+
+//         if (currentSortDirectionTitle === 'asc') {
+//             if (titleA < titleB) {
+//                 return -1;
+//             }
+//             if (titleA > titleB) {
+//                 return 1;
+//             }
+//         } else {
+//             if (titleA < titleB) {
+//                 return 1;
+//             }
+//             if (titleA > titleB) {
+//                 return -1;
+//             }
+//         }
+
+//         return 0;
+//     };
+
+//     const sortDirection = sortSelect.value;
+//     const rows = Array.from(table.tBodies[0].rows);
+
+//     rows.sort(sortByTitle);
+
+//     if (sortDirection === 'desc') {
+//         rows.reverse();
+//         currentSortDirectionTitle = 'desc'; // Update the sorting direction for title column
+//     } else {
+//         currentSortDirectionTitle = 'asc'; // Update the sorting direction for title column
+//     }
+
+//     for (const row of rows) {
+//         table.tBodies[0].appendChild(row);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// let currentSortDirection = 'asc'; 
+// function sortTableAuthor() {
+//     console.log('sortTable function called');
+//     const table = document.querySelector('.table');
+//     const sortSelect = document.getElementById('sort');
+
+//     const sortByAuthor = (a, b) => {
+//         const authorA = a.cells[1].textContent.toLowerCase();
+//         const authorB = b.cells[1].textContent.toLowerCase();
+//         if (currentSortDirection === 'asc') {
+//             if (authorA < authorB) {
+//                 return -1;
+//             }
+//             if (authorA > authorB) {
+//                 return 1;
+//             }
+//         } else {
+//             if (authorA < authorB) {
+//                 return 1;
+//             }
+//             if (authorA > authorB) {
+//                 return -1;
+//             }
+//         }
+
+//         return 0;
+//     };
+
+//     const sortDirection = sortSelect.value;
+//     const rows = Array.from(table.tBodies[0].rows); 
+
+//     rows.sort(sortByAuthor);
+
+//     if (sortDirection === 'desc') {
+//         rows.reverse();
+//     }
+
+//     for (const row of rows) {
+//         table.tBodies[0].appendChild(row); 
+//     }
+// }
+
+// function sortTableTitle() {
+//     console.log('sortTable function called');
+//     const table = document.querySelector('.table'); 
+//     const sortSelect = document.getElementById('sort');
+
+//     const sortByTitle = (a, b) => {
+//         const titleA = a.cells[0].textContent.toLowerCase();
+//         const titleB = b.cells[0].textContent.toLowerCase();
+
+//         if (titleA < titleB) {
+//             return -1;
+//         }
+//         if (titleA > titleB) {
+//             return 1;
+//         }
+//         return 0;
+//     };
+
+//     const sortDirection = sortSelect.value;
+//     const rows = Array.from(table.tBodies[0].rows);
+
+//     rows.sort(sortByTitle);
+
+//     if (sortDirection === 'desc') {
+//         rows.reverse();
+//     }
+
+//     for (const row of rows) {
+//         table.tBodies[0].appendChild(row); 
+//     }
+// }
 
 
--- src/resources/views/add-book-form.blade.php
+++ src/resources/views/add-book-form.blade.php
@@ -5,67 +5,69 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="{{ asset('script.js') }}"></script>
-  </head>
+</head>
 <body>
-  <div class="container mt-4">
+<div class="container mt-4">
     <div class="card">
-      <div class="card-header text-center font-weight-bold">
-        Add Book
-      </div>
-      <div class="card-body">
-        <form method="POST" action="{{ route('store-book') }}"> 
-          @csrf
-          <div class="form-group">
-            <label for="title">Title:</label>
-            <input type="text" name="title" id="title" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="author">Author:</label>
-            <input type="text" name="author" id="author" class="form-control" required>
-          </div>
-          <button type="submit" class="btn btn-primary">Add Book</button>
-        </form>
-      </div>
-    </div>
-    
-    <div class="mt-4">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th>
-              <label for="sort">Title:</label>
-              <select id="sort" name="sort" onchange="sortTableTitle()" class="form-control"  style="width: 60px; display: inline-block; margin-left: 10px;">
-                <option value="asc">A-Z</option>
-                <option value="desc">Z-A</option>
-              </select>
-            </th>
-            <th>
-              <label for="sort">Author:</label>
-              <select id="sort" name="sort" onchange="sortTableAuthor()" class="form-control"  style="width: 60px; display: inline-block; margin-left: 10px;">
-                <option value="asc">A-Z</option>
-                <option value="desc">Z-A</option>
-              </select>
-            </th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($books as $book)
-          <tr>
-            <td>{{ $book->title }}</td>
-            <td>{{ $book->author }}</td>
-            <td>
-            <form method="POST" action="{{ route('books.delete', $book->id) }}">
-              @csrf 
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">Delete</button>
+        <div class="card-header text-center font-weight-bold">
+            Add Book
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('store-book') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" name="title" id="title" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="author">Author:</label>
+                    <input type="text" name="author" id="author" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Add Book</button>
             </form>
-            </td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+        </div>
     </div>
-  </div>  
+
+    <div class="mt-4">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+              <th>
+                <label for="sort">Title:</label>
+                <select id="sort" name="sort" onchange="sortTable()">
+                  <option value="asc">A-Z</option>
+                  <option value="desc">Z-A</option>
+                  </select>
+                </th>
+                <th>
+                    <label for="sort">Author:</label>
+                    <select id="sort" name="sort" onchange="sortTable()">
+                        <option value="asc">A-Z</option>
+                        <option value="desc">Z-A</option>
+                    </select>
+                </th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($books as $book)
+                <tr>
+                    <td>{{ $book->title }}</td>
+                    <td>{{ $book->author }}</td>
+                    <td>
+                        <form method="POST" action="{{ route('books.delete', $book->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
