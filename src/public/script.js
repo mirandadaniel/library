c558169c0e7841dedcc8e0e050ac419806fa5f77
mirandadaniel <78826776+mirandadaniel@users.noreply.github.com>
2023-09-30 11:13:51 +0900

    removing old comments

--- src/public/script.js
+++ src/public/script.js
@@ -86,18 +86,15 @@ $('.editable-cell').click(function () {
     const bookId = cell.data('id');
     const field = cell.data('field');
     const currentValue = cell.text();
-    
-    // Prompt the user for a new value
     const newValue = prompt(`Edit ${field}:`, currentValue);
     
     if (newValue !== null && newValue !== currentValue) {
-        // Check if the new value is empty
+
         if (newValue.trim() === '') {
             alert('Value cannot be left blank');
-            return; // Do not proceed with the update
+            return; 
         }
 
-        // Proceed with the update if the new value is not empty
         updateBookField(bookId, field, newValue);
     }
 });
